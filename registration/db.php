71a79f2f7658f07475dc6b03ae5907997494e284
Fransisca Julia K. D <frjulia.julia@gmail.com>
2016-03-21 06:20:16 +0700

    coba user registration
    belum begitu paham pake mysqli , masih ada eror, jadi belum diconnectin
    ke satya huf

---
+++ registration/db.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Author: Javed Ur Rehman
+Website: https://htmlcssphptutorial.wordpress.com
+*/
+?>
+
+<?php
+$connection = mysqli_connect('localhost', 'root', '', 'register');
+if (!$connection){
+    die("Database Connection Failed" . mysql_error());
+}
+
+$select_db = mysqli_select_db( $connection, "register");
+if (!$select_db){
+    die("Database Selection Failed" . mysql_error());
+}
+?>
