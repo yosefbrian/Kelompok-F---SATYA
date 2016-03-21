ab55d711075d209d0a221857422f378c50c40dc4
Fransisca Julia K. D <frjulia.julia@gmail.com>
2016-03-21 21:59:56 +0700

    satya-login
    uyeah

---
+++ satya-login/resources/lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Sandi minimal terdiri dari 6 karakter.',
+    'reset' => 'Sandi telah diubah',
+    'sent' => 'Kami telah mengirim link untuk mengubah sandi ke e-mail anda',
+    'token' => 'Token ubah sandi ini tidak valid',
+    'user' => "Kami tidak dapat menemukan user dengan alamat e-mail ini",
+
+];
